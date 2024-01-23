c384c3372b67179ed7ec91b472f6c3249a0d0316
David <davidsalem37@gmail.com>
2024-01-23 23:02:46 +0200

    Actualizar loadPath para GitHub Pages

--- src/i18n.js
+++ src/i18n.js
@@ -1,8 +1,10 @@
+// Importa las bibliotecas necesarias
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import HttpApi from 'i18next-http-backend';
 
+// Configuración de i18next
 i18n
   .use(HttpApi)
   .use(LanguageDetector)
@@ -10,6 +12,9 @@ i18n
   .init({
     debug: true,
     fallbackLng: 'en',
+    backend: {
+      loadPath: '/locales/{{lng}}/{{ns}}.json', // Ajusta la ruta de carga según tu estructura de archivos
+    },
   });
 
 export default i18n;
