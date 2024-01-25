02494600ce51ca12460d8ace9f9605e8e860aeb8
David <davidsalem37@gmail.com>
2024-01-25 22:25:19 +0200

    i18n path cv

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,34 +1,39 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 import "./About.css";
 
-export const About = ({ t }) => {
-  const handleDownload = () => {
-    const cvPath = t("aboutSection.pathCV");
+export const About = () => {
+  const { t } = useTranslation();
 
-    const link = document.createElement('a');
+  const handleDownload = () => {
+    const cvPath = `${process.env.PUBLIC_URL}/${t("aboutSection.pathCV")}`;
+
+    const link = document.createElement("a");
     link.href = cvPath;
-    link.download = 'Resume.pdf';
+    link.download = "Resume.pdf";
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
   };
 
   return (
-    <section className="about_container container section" id='about'>
-      <h2 className="section_title">{t('aboutSection.title')}</h2>
+    <section className="about_container container section" id="about">
+      <h2 className="section_title">{t("aboutSection.title")}</h2>
 
-        <div className="about_data grid">
-          <div className="about_info">
-            <p className="about_description">
-              {t('aboutSection.description.paragraph1')} <br /><br />
-              {t('aboutSection.description.paragraph2')} <br /><br />
-              {t('aboutSection.description.paragraph3')}
-            </p>
-            <button className="btn btn_resume" onClick={handleDownload}>
-              {t('aboutSection.downloadCV')}
-            </button>
-          </div>
+      <div className="about_data grid">
+        <div className="about_info">
+          <p className="about_description">
+            {t("aboutSection.description.paragraph1")} <br />
+            <br />
+            {t("aboutSection.description.paragraph2")} <br />
+            <br />
+            {t("aboutSection.description.paragraph3")}
+          </p>
+          <button className="btn btn_resume" onClick={handleDownload}>
+            {t("aboutSection.downloadCV")}
+          </button>
         </div>
+      </div>
     </section>
   );
 };
