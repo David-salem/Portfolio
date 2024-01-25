b4eaed0c090da064b20cd26456afced534ed96ac
David <davidsalem37@gmail.com>
2024-01-25 18:03:41 +0200

    Resume path fixed

--- src/i18n.js
+++ src/i18n.js
@@ -1,10 +1,8 @@
-// Importa las bibliotecas necesarias
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import HttpApi from 'i18next-http-backend';
 
-// Configuración de i18next
 i18n
   .use(HttpApi)
   .use(LanguageDetector)
@@ -12,9 +10,12 @@ i18n
   .init({
     debug: true,
     fallbackLng: 'en',
-    backend: {
-      loadPath: '/locales/{{lng}}/{{ns}}.json', // Ajusta la ruta de carga según tu estructura de archivos
+    interpolation: {
+      escapeValue: false,
     },
-  });
+    backend: {
+      loadPath: '/david-salem/locales/{{lng}}/{{ns}}.json',
+    },
+  })
 
 export default i18n;
